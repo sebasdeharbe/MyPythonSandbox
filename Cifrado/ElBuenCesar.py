35870f2c979df01dfb7abafdd0d13005f4279b7f
Sebastian Deharbe <sebasdeharbe@gmail.com>
2023-09-08 19:44:56 -0300

    changes-elbuencesar.py

--- Cifrado/ElBuenCesar.py
+++ Cifrado/ElBuenCesar.py
@@ -27,3 +27,8 @@ texto_cifrado = "pfx htrywfxjsfx xtr htqt pf wtuf nryjwntw rt uzjijx ijñfw vzj 
 clave = 5
 mensaje_descifrado = descifrar_cesar(texto_cifrado, clave, "abcdefghijklmnñopqrstuvwxyz")
 print(mensaje_descifrado)
+print('-------------------------------------------')
+texto_cifrado = "pfnybfvrc dvderzn p.z. yn rerc,vqnql bfr arczver n y.d u.zocrd ryrtvc df ac.av. sfefc.l erczv,ncá ryvtvr,q. yn drtfcvqnq j yn zrqv.pcvqnql j r, rdn crnyvqnq ynd rdecryynd rdeá, sfrcn qry nypn,prm yn zrcn rivder,pvn qr yn rerc,vqnq ryvzv,n qr f, ayfznk. ry vzarcv. tnyápevp.m ancn crdenfcncy.l qror npnoncdr p., yn rerc,vqnqm"
+clave = 13
+mensaje_descifrado = descifrar_cesar(texto_cifrado, clave, ".abcdefghijklmnopqrstuvwxyz,")
+print(mensaje_descifrado)
